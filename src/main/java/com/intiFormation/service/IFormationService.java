4543578fb2f052cfae2741d5094a9b17a4892c05
scheraz99 <sherazbettayeb@gmail.com>
2022-12-15 14:17:45 +0100

    ajout d'un attribut formateur à une formation

--- src/main/java/com/intiFormation/service/IFormationService.java
+++ src/main/java/com/intiFormation/service/IFormationService.java
@@ -3,6 +3,7 @@ package com.intiFormation.service;
 import java.util.List;
 
 import com.intiFormation.model.Formation;
+import com.intiFormation.model.Utilisateur;
 
 public interface IFormationService {
 	
